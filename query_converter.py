fdd26b54413d88e3bf8a6607fe2bef08b62f0eea
Steph Gooch <awssteph@amazon.com>
2024-08-05 18:59:23 +0100

    query runs but needs titles back in

--- query_converter.py
+++ query_converter.py
@@ -10,51 +10,27 @@ special_columns_keep = product_columns_keep + discount_columns_keep
 unchanged_prefix = ("line_item", "identity", "bill", "reservation", "savings_plan", "reservation", "pricing", "split_line_item")
     
 
-def old():
-      
-    for column in column_list:
-        if column.startswith(unchanged_prefix):
-            CUR2_list.append(column)
-            
-        elif column in special_columns_keep:
-            CUR2_list.append(column) 
-            
-        elif column.startswith("product"):
-            new_col = column.replace("product_", "product.", 1)
-            CUR2_list.append(new_col + " AS " + column)
-            
-        elif column.startswith("cost_category_"):
-            new_col = column.replace("cost_category_", "cost_category.", 1)
-            CUR2_list.append(new_col + " AS " + column)
-            
-        elif column.startswith("resource_tags_"):
-            new_col = column.replace("resource_tags_", "resource_tags.", 1)
-            CUR2_list.append(new_col + " AS " + column)
-            
-        elif column.startswith("discount_"):
-            new_col = column.replace("discount_", "discount.", 1)
-            CUR2_list.append(new_col + " AS " + column)
 
 def change_sql():
     reading_file = open("test.txt", "r")
 
     new_file_content = ""
-    
+
     for line in reading_file:
         new_line = line.strip()
-        
         new_line = new_line.replace(';', '')
-        if "product" in new_line:    
-            new_line = new_line.replace(',','')       
-            if new_line not in product_columns_keep:
+        #import pdb; pdb.set_trace()
+        noncom_new_line = new_line.replace(',','') 
+        if noncom_new_line not in product_columns_keep:
+            if new_line.startswith('product'):
                 new_line = new_line[8:]
-                #import pdb; pdb.set_trace()
                 if ',' in new_line:
-                    
+                    new_line = new_line.replace(',','')  
+
                     new_line = f"product['{new_line}'],"
                 else : new_line = f"product['{new_line}']"
-                
-            new_file_content += new_line +",\n"
+            
+            new_file_content += new_line +"\n"
         else:
             new_file_content += new_line +"\n"
     reading_file.close()
@@ -63,4 +39,4 @@ def change_sql():
     writing_file.write(new_file_content)
     writing_file.close()
 
-change_sql()
+change_sql()#

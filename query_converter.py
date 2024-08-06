47762c2a5346825de3f32ed62ac2d7265815a8e4
Steph Gooch <awssteph@amazon.com>
2024-08-06 10:11:26 +0100

    added args

--- query_converter.py
+++ query_converter.py
@@ -1,8 +1,11 @@
-#####################################
-# Will not work with nested queries #
-# save query in test.text           #
-#>> python query_converter.py       #
-#####################################
+##########################################
+# Will not work with nested queries      #
+# save query in test.text                #
+#>> python query_converter.py  <file.txt>#
+##########################################
+
+import sys
+
 product_columns_keep=["product", "product_sku", "product_comment","product_fee_code","product_fee_description","product_from_location","product_from_location_type","product_from_region_code","product_instance_family","product_instance_type","product_instanceSKU","product_location","product_location_type","product_operation","product_pricing_unit","product_product_family","product_region_code","product_servicecode","product_to_location","product_to_location_type","product_to_region_code","product_usagetype"]
 #the product columns from CUR that are kept in CUR 2.0
 
@@ -14,7 +17,11 @@ special_columns_keep = product_columns_keep + discount_columns_keep
 unchanged_prefix = ("line_item", "identity", "bill", "reservation", "savings_plan", "reservation", "pricing", "split_line_item")
 
 def change_sql():
-    reading_file = open("test.txt", "r")
+    try: 
+        param_1= sys.argv[1]
+    except: 
+        param_1 = "test.txt"
+    reading_file = open(param_1, "r")
 
     new_file_content = ""
     colname = True
